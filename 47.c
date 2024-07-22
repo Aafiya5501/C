b2f16203e14df4bba4b5d808b8def372e48be312
aafiya <146860941+Aafiya5501@users.noreply.github.com>
2024-07-22 19:23:01 +0530

    Add files via upload

---
+++ 47.c
@@ -0,0 +1,10 @@
+#include<math.h>
+void main(){
+	int i=0;
+	while(i<=9)
+	{
+		printf("%d %f",i,sqrt(i));
+		i=i+1;
+		
+	}
+}
---
+++ 48.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+	int i=101,n;
+	while(i<200)
+	{
+		if(i%7==0&&i%5!=0){
+		printf("%d",i);}
+		i=i+1;
+	
+}
+}
+
